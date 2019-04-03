a8022268478bca0b57185d5d96627d91edff6da8
d.kachurinets <d.kachurinets@gmail.com>
2019-04-03 11:46:35 +0300

    crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { MatButtonModule, MatCardModule, MatExpansionModule, MatInputModule, MatToolbarModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatExpansionModule,
+  MatInputModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule
+} from '@angular/material';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -29,6 +36,7 @@ import { AppRoutingModule } from "./app-routing.module";
     MatButtonModule,
     MatToolbarModule,
     MatExpansionModule,
+    MatProgressSpinnerModule,
     HttpClientModule,
   ],
   providers: [],
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,31 +1,37 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Post } from '../post.model';
 import { PostService } from '../post.service';
-import { Subscription} from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-post-list',
-  templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.css']
 })
 
-export class PostListComponent implements OnInit, OnDestroy{
-  posts: Post[] = [];
-  private postsSub: Subscription;
-  constructor(public postsService: PostService) {}
+export class PostListComponent implements OnInit, OnDestroy {
+    posts: Post[] = [];
+    private postsSub: Subscription;
+    isLoading = false;
 
-  ngOnInit() {
-   this.postsService.getPosts();
-   this.postsSub = this.postsService.getPostUpdateListener()
-     .subscribe((posts: Post[]) => {
-       this.posts = posts;
-     });
-  }
-  onDelete(id: string) {
-    this.postsService.deletePost(id);
-  }
+    constructor(public postsService: PostService) {
+    }
 
-  ngOnDestroy() {
-    this.postsSub.unsubscribe();
-  }
+    ngOnInit() {
+        this.isLoading = true;
+        this.postsService.getPosts();
+        this.postsSub = this.postsService.getPostUpdateListener()
+            .subscribe((posts: Post[]) => {
+                this.isLoading = false;
+                this.posts = posts;
+            });
+    }
+
+    onDelete(id: string) {
+        this.postsService.deletePost(id);
+    }
+
+    ngOnDestroy() {
+        this.postsSub.unsubscribe();
+    }
 }
